ad9080787bf14cada1e73f880c66ab40b0bd0f7d
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-06-06 17:04:41 +0530

    Update index.js
    react extnsn

--- packages/react-devtools/index.js
+++ packages/react-devtools/index.js
@@ -8,7 +8,7 @@
  */
 
 const {connectToDevTools} = require('react-devtools-core/backend');
-
+ 
 // Connect immediately with default options.
 // If you need more control, use `react-devtools-core` directly instead of `react-devtools`.
 connectToDevTools();
